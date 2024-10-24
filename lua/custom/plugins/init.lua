e0239dbaa9e80b126f67ae042387ecefdc34e1bd
johanw1232 <73111635+johanw1232@users.noreply.github.com>
2024-10-24 16:20:01 +0200

    changed to roslyn.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -40,10 +40,42 @@ return {
     'Hoffs/omnisharp-extended-lsp.nvim',
   },
 
+  -- {
+  --   --Before trying to use the language server you should install it. You can do so by running :CSInstallRoslyn which will install the configured version of the plugin in neovim's datadir.
+  --   'jmederosalvarado/roslyn.nvim',
+  -- },
   {
-    --Before trying to use the language server you should install it. You can do so by running :CSInstallRoslyn which will install the configured version of the plugin in neovim's datadir.
-    'jmederosalvarado/roslyn.nvim',
+    'seblj/roslyn.nvim',
+    ft = 'cs',
+    opts = {
+      config = {
+        settings = {
+          ['csharp|inlay_hints'] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+          },
+          ['csharp|code_lens'] = {
+            dotnet_enable_references_code_lens = true,
+          },
+        },
+      },
+      exe = {
+        'dotnet',
+        vim.fs.joinpath(vim.fn.stdpath 'data', 'roslyn', 'Microsoft.CodeAnalysis.LanguageServer.dll'),
+      },
+    },
   },
+
   {
     'NeogitOrg/neogit',
     dependencies = {
